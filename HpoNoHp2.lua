f024b64be0f71f2c7bc87162181e730c429f077d
EzkieMalia <ezekielquisky1z@outlook.com>
2025-11-26 17:44:45 -0700

    Update HpoNoHp2.lua

--- HpoNoHp2.lua
+++ HpoNoHp2.lua
@@ -1,1 +1,54 @@
+local Dark = game.Players.LocalPlayer
+local PlayerGui = Dark.PlayerGui
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = PlayerGui
+
+task.wait(.1)
+print("#----------||---------#")
+task.wait(.1)
+print("##---------||---------##")
+task.wait(.1)
+print("###--------||--------###")
+task.wait(.1)
+print("####-------||-------####")
+task.wait(.1)
+print("#####------||------#####")
+task.wait(.1)
+print("######-----||-----######")
+task.wait(.1)
+print("#######----||----#######")
+task.wait(.1)
+print("########---||---########")
+task.wait(.1)
+print("#########--||--#########")
+task.wait(.1)
+print("##########-||-##########")
+task.wait(.1)
+print("###########--###########")
+task.wait(.1)
+print("########################")
+task.wait(.1)
 print("Sucessfully Loaded Script.")
+task.wait(.5)
+print("WAIT FOR EVERYTHING TO LOAD. ~1 MINUTE.")
+task.wait(30)
+local Frame = Instance.new("Frame")
+Frame.Parent = ScreenGui
+Frame.Size = UDim2.new(0.5, 0, 0.5, 0)
+Frame.Position = UDim2.new(.25, 0, .25, 0)
+Frame.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+
+local FrameStroke = Instance.new("UIStroke")
+FrameStroke.Parent = Frame
+FrameStroke.Thickness = 2
+
+local FrameCorner = Instance.new("UICorner")
+FrameCorner.Parent = Frame
+FrameCorner.CornerRadius = UDim.new(.01)
+
+
+
+
+
+
