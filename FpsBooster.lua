2a6ef43d682fd30bf13d1f9c257d9912e8d3a016
EzkieMalia <ezekielquisky1z@outlook.com>
2025-11-28 19:50:05 -0700

    Update FpsBooster.lua

--- FpsBooster.lua
+++ FpsBooster.lua
@@ -39,15 +39,15 @@ if ANTICHEATDETECTION <= 5 then
      local Random = Random.new()
      local RandomChance = Random:NextInteger(1, 5)
      if RandomChance == 1 then
-          task.wait(540)
+          task.wait(600)
           game.Players.LocalPlayer:Kick("Exploiting") elseif RandomChance == 2 then
-          task.wait(480)
+          task.wait(540)
           game.Players.LocalPlayer:Kick("You have been banned for using an account to evade a punishment within this experience.") elseif RandomChance == 3 then
-          task.wait(420)
+          task.wait(480)
           game.Players.LocalPlayer:Kick("Exploiting") elseif RandomChance == 4 then
-          task.wait(360)
+          task.wait(420)
           game.Players.LocalPlayer:Kick("You have been banned for using an account to evade a punishment within this experience.") elseif RandomChance == 5 then
-          task.wait(300)
+          task.wait(360)
           game.Players.LocalPlayer:Kick("Expoiting")
      end
 end
